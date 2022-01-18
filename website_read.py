514138f1aea0770df2c612f6e2c6bfbe805256a3
Aiden Mitchell <me@aidenmitchell.ca>
2022-01-17 16:04:08 -0800

    Add files via upload

---
+++ website_read.py
@@ -0,0 +1,48 @@
+import requests
+from bs4 import BeautifulSoup
+import feedparser
+from datetime import datetime
+import re
+
+
+def aws():
+    link = "https://aws-status.info/"
+    html = requests.get(link).text
+    soup = BeautifulSoup(html, "lxml")
+
+    regions = ["North America", "South America", "Europe", "Asia Pacific"]
+    statuses = soup.findAll("span", {'class': 'label'})
+    messages = soup.findAll("span", {'class': 'message'})
+
+    for region in regions:
+        if statuses[regions.index(region)].text != "All services are operating normally":
+            return ("Issue in " + region + ": " + statuses[regions.index(region)].text + "\nIssue: " + messages[
+                0].text)
+    return "All services are operating normally"
+
+
+def cloudflare():
+    link = "https://www.cloudflarestatus.com/history.atom"
+    feed = feedparser.parse(link)
+    issues = []
+    date = datetime.today().strftime('%Y-%m-%d')
+    for entry in feed["entries"]:
+        if date in entry["updated"]:
+            issues.append(entry["title"])
+            issues.append(entry["link"])
+    return str(issues)
+
+
+def google_cloud():
+    link = "https://status.cloud.google.com/en/feed.atom"
+    feed = feedparser.parse(link)
+    issues = []
+    for entry in feed["entries"]:
+        issues.append(entry["title"])
+        issues.append(entry["link"])
+    return str(issues)
+
+
+print("### AWS STATUS ### \n" + aws() + "\n")
+print("### CLOUDFLARE STATUS ### \n" + cloudflare() + "\n")
+print("### GOOGLE CLOUD STATUS ### \n" + google_cloud())
