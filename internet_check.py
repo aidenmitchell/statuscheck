4c4b9558ca5fc1521592a27e52bfe00bf7272e81
aiden.mitchell <me@aidenmitchell.ca>
2022-01-18 13:30:32 -0800

    Added ping tests

---
+++ internet_check.py
@@ -0,0 +1,18 @@
+from ping3 import ping
+
+
+def pinger(host):
+    ping_time = ping(host)
+    return str(ping_time * 1000)[0:5]  # in ms
+
+
+def multi_ping(hosts):
+    ping_results = {}
+    for host in hosts:
+        ping_results[host] = pinger(host)
+    return ping_results
+
+
+results = multi_ping(['1.1.1.1', '8.8.8.8'])
+for host, result in results.items():
+    print(f'{host} {result} ms')
